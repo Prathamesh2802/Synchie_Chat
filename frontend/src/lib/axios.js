b3358f9586849ebece4d8795cf6bde109fa463aa
Prathamesh2802 <pinupatil820@gmail.com>
2026-04-19 00:54:24 +0530

    axios bug fix api backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000/api",
+  // baseURL: "http://localhost:9000/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+
   withCredentials: true, // with crediantials parameter used for auth cookies
 });
